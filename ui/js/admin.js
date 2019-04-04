a0001c22941ec1db773803ce8d2653f7d659f1a6
adafia <adafia.samuel@gmail.com>
2019-04-04 12:16:07 +0200

    Update develop with mobile responsiveness changes

--- ui/js/admin.js
+++ ui/js/admin.js
@@ -1,6 +1,12 @@
 let modalBtn = document.querySelector('.modal-btn');
 let modal = document.querySelector('.modal');
 
+let approvalsMTop = document.querySelector('.approvals-m-top');
+let adminMTop = document.querySelector('.admin-m-top');
+let staffMTop = document.querySelector('.staff-m-top');
+let bankMTop = document.querySelector('.bank-m-top');
+let transactMTop = document.querySelector('.transact-m-top');
+
 let approvalsM = document.querySelector('.approvals-m');
 let adminM = document.querySelector('.admin-m');
 let staffM = document.querySelector('.staff-m');
@@ -22,6 +28,46 @@ console.log(tr);
 modalBtn.addEventListener('click', function() {
     // modal.style.display = 'none';
     modal.classList.add('hide');
+});
+
+approvalsMTop.addEventListener('click', function() {
+    approvalsContent.classList.remove('hide');
+    adminContent.classList.add('hide');
+    staffContent.classList.add('hide');
+    bankContent.classList.add('hide');
+    transactionsContent.classList.add('hide');
+});
+
+adminMTop.addEventListener('click', function() {
+    approvalsContent.classList.add('hide');
+    adminContent.classList.remove('hide');
+    staffContent.classList.add('hide');
+    bankContent.classList.add('hide');
+    transactionsContent.classList.add('hide');
+});
+
+staffMTop.addEventListener('click', function() {
+    approvalsContent.classList.add('hide');
+    adminContent.classList.add('hide');
+    staffContent.classList.remove('hide');
+    bankContent.classList.add('hide');
+    transactionsContent.classList.add('hide');
+});
+
+bankMTop.addEventListener('click', function() {
+    approvalsContent.classList.add('hide');
+    adminContent.classList.add('hide');
+    staffContent.classList.add('hide');
+    bankContent.classList.remove('hide');
+    transactionsContent.classList.add('hide');
+});
+
+transactMTop.addEventListener('click', function() {
+    approvalsContent.classList.add('hide');
+    adminContent.classList.add('hide');
+    staffContent.classList.add('hide');
+    bankContent.classList.add('hide');
+    transactionsContent.classList.remove('hide');
 });
 
 approvalsM.addEventListener('click', function() {
