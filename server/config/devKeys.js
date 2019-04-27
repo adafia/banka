4f32327f5c0912e0455f5cd7569bbeb5664b39d1
adafia <adafia.samuel@gmail.com>
2019-04-27 10:06:49 +0200

    fix travis

--- server/config/devKeys.js
+++ server/config/devKeys.js
@@ -1,4 +1,7 @@
-module.exports =  {
-    DATABASE_URL: 'postgres://postgres:12345@127.0.0.1:5432/banka_db',
-    SECRET_OR_KEY: 'gameofthroneswinterishere' 
+import dotenv from 'dotenv';
+dotenv.config()
+
+module.exports = {
+    DATABASE_URL: process.env.DEV_DATABASE_URL,
+    SECRET_OR_KEY: process.env.SECRET_OR_KEY 
 }
--- server/config/testKeys.js
+++ server/config/testKeys.js
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
 module.exports = {
-    DATABASE_URL: 'postgres://postgres:12345@127.0.0.1:5432/banka_test_db',
-    SECRET_OR_KEY: 'gameofthroneswinterishere' 
+    DATABASE_URL: process.env.TEST_DATABASE_URL,
+    SECRET_OR_KEY: process.env.SECRET_OR_KEY 
 }
